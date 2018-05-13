0011436e396dc07de66d0a5f5305c848bceebd7d
Kevin Caccamo <kevin@ciinet.org>
2018-05-13 17:18:57 -0400

    Fix DOM nesting

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ class App extends React.Component {
       <div className="App">
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
-          <PolynomialTestbed/>
         </p>
+        <PolynomialTestbed/>
       </div>
     );
   }
