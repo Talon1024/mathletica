a24fa7ce5079e1ec936b84389a9fa24862af76a5
Kevin Caccamo <kevin@ciinet.org>
2018-09-14 11:17:29 -0400

    Add more info to comment on terms property

--- src/Questions/Generators/Polynomial.ts
+++ src/Questions/Generators/Polynomial.ts
@@ -4,7 +4,7 @@ import { MathQuestion } from './Base/MathQuestion';
 
 export class PolynomialQuestion extends MathQuestion {
   public termName = "x";
-  protected terms:number[]; // Subscript is power, value is multiplier (e.g. terms[0] = 2 -> 2, terms[1] = 3 -> 3x)
+  protected terms:number[]; // Subscript is power of x, value is multiplier (e.g. terms[0] = 2 -> 2(x^0=1), terms[1] = 3 -> 3x^1)
   protected readonly MAX_TERMS = [1, 2, 3];
   protected lhs:number;
 
